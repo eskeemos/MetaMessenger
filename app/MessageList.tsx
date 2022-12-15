7755c0cb64ab3ec2dc28eedc8ae0f1257fcb55e7
eskeemos <l.michalik004@gmail.com>
2022-12-15 10:08:16 +0100

    maxRetriesError

---
+++ app/MessageList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MessageList() {
+  return (
+    <div>
+      <p>message</p>
+      <p>message</p>
+      <p>message</p>
+      <p>message</p>
+      <p>message</p>
+    </div>
+  )
+}
+
+export default MessageList
